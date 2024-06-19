9eb7d547e1a9f0a0989deb6e2466700bf74bd55c
Datseris <datseris.george@gmail.com>
2024-06-19 17:10:05 +0100

    delete all files from template; update to correct names

--- src/PeriodicOrbits.jl
+++ src/PeriodicOrbits.jl
@@ -1,29 +1,6 @@
 module PeriodicOrbits
 
 using Reexport
-@reexport using StateSpaceSets
-
-import SciMLBase
-import SciMLBase: reinit!, step!, isinplace
-using SciMLBase: recursivecopy
-export reinit!, step!, isinplace, recursivecopy
-
-include("core/dynamicalsystem_interface.jl")
-include("core/trajectory.jl")
-include("core/utilities.jl")
-include("core/pretty_printing.jl")
-
-include("core_systems/discrete_time_map.jl")
-include("core_systems/continuous_time_ode.jl")
-include("core_systems/arbitrary_steppable.jl")
-include("core_systems/additional_supertypes.jl")
-
-include("derived_systems/stroboscopic_map.jl")
-include("derived_systems/parallel_systems.jl")
-include("derived_systems/tangent_space.jl")
-include("derived_systems/poincare/poincaremap.jl")
-include("derived_systems/projected_system.jl")
-
-include("deprecations.jl")
+@reexport using DynamicalSystemsBase
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,9 @@
-using DynamicalSystemsBase
+using PeriodicOrbits
 using Test
 
 defaultname(file) = uppercasefirst(replace(splitext(basename(file))[1], '_' => ' '))
 testfile(file, testname=defaultname(file)) = @testset "$testname" begin; include(file); end
 
-@testset "DynamicalSystemsBase" begin
-    testfile("discrete.jl")
-    testfile("continuous.jl")
-    testfile("arbitrarysteppable.jl")
-    testfile("stroboscopic.jl")
-    testfile("parallel.jl")
-    testfile("tangent.jl")
-    testfile("poincare.jl")
-    testfile("projected.jl")
-    testfile("successful_step.jl")
-    testfile("mtk_integration.jl")
-    testfile("trajectory.jl")
+@testset "PeriodicOrbits" begin
+    @test true
 end
