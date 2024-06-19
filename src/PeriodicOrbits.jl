d1529869515c00f30fd0d56362b0c2f1312739d3
George Datseris <datseris.george@gmail.com>
2024-06-19 17:03:20 +0100

    Rename to PeriodicOrbits (#1)
    * rename project to periodic orbits
    
    * Update and rename DynamicalSystemsBase.jl to PeriodicOrbits.jl

--- src/PeriodicOrbits.jl
+++ src/PeriodicOrbits.jl
@@ -1,11 +1,4 @@
-module DynamicalSystemsBase
-
-# Use the README as the module docs
-@doc let
-    path = joinpath(dirname(@__DIR__), "README.md")
-    include_dependency(path)
-    read(path, String)
-end DynamicalSystemsBase
+module PeriodicOrbits
 
 using Reexport
 @reexport using StateSpaceSets
