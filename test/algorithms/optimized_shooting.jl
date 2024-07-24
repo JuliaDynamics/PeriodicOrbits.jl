e77c7c44e3dcc05d51cce4bf27d297521101325e
Jonas Koziorek <jonas.koziorek@gmail.com>
2024-07-24 18:32:00 +0200

    update tests

--- test/algorithms/optimized_shooting.jl
+++ test/algorithms/optimized_shooting.jl
@@ -1,6 +1,5 @@
 using Test
 using PeriodicOrbits
-using OrdinaryDiffEq: RKO65
 using LinearAlgebra: norm
 
 @inbounds function lorenz_rule(u, p, t)
@@ -11,20 +10,19 @@ using LinearAlgebra: norm
 end
 
 @testset "Optimized shooting" begin
-    ds = CoupledODEs(lorenz_rule, [0.0, 10.0, 0.0], [10.0, 28.0, 8 / 3])
     igs = [InitialGuess(SVector(1.0, 2.0, 5.0), 4.2), InitialGuess(SVector(1.0, 2.0, 5.0), 5.2)]
     ig = igs[1]
     alg = OptimizedShooting(Δt=1e-3, n=3, abstol=1e-6, optim_kwargs=(f_tol=1e-10,))
-    ds = CoupledODEs(lorenz_rule, [0.0, 10.0, 0.0], [10.0, 28.0, 8 / 3]; diffeq=(alg=RKO65(), abstol=1e-14, reltol=1e-14, dt=alg.Δt))
+    ds = CoupledODEs(lorenz_rule, [0.0, 10.0, 0.0], [10.0, 28.0, 8 / 3]; diffeq=(abstol=1e-14, reltol=1e-14))
     res = periodic_orbit(ds, alg, ig)
     @test !isnothing(res)
-
-    ds2 = CoupledODEs(lorenz_rule, [0.0, 10.0, 0.0], [10.0, 28.0, 8 / 3]; diffeq=(alg=RKO65(), abstol=1e-14, reltol=1e-14, dt=1e-6)) # initialize again with smaller step size
-    u0 = res.points[1]
-    T = res.T
-    reinit!(ds2, u0)
-    step!(ds2, T)
-    @test norm(current_state(ds2) - u0) <= 1e-2 # these POs are unstable and so the trajectories are diverging
+    if !isnothing(res)
+        u0 = res.points[1]
+        T = res.T
+        reinit!(ds, u0)
+        step!(ds, T, true)
+        @test norm(current_state(ds) - u0) <= 1e-6
+    end
 
     res = periodic_orbits(ds, alg, igs)
     @test length(res) == 2
@@ -32,8 +30,8 @@ end
     for po in res
         u0 = po.points[1]
         T = po.T
-        reinit!(ds2, u0)
-        step!(ds2, T)
-        @test norm(current_state(ds2) - u0) <= 1e-2
+        reinit!(ds, u0)
+        step!(ds, T, true)
+        @test norm(current_state(ds) - u0) <= 1e-6
     end
 end
