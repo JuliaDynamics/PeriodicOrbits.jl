c8251a37119022e68b20dc1a520dc4d68cdac6a8
Jonas Koziorek <jonas.koziorek@gmail.com>
2024-08-07 20:34:21 +0200

    changes from the meeting

--- src/algorithms/optimized_shooting.jl
+++ src/algorithms/optimized_shooting.jl
@@ -1,6 +1,5 @@
-export periodic_orbit, periodic_orbits, OptimizedShooting, periodic_orbit2, periodic_orbits2
+export periodic_orbit, periodic_orbits, OptimizedShooting
 
-using LeastSquaresOptim: optimize, LevenbergMarquardt
 using NonlinearSolve
 
 
@@ -8,80 +7,87 @@ using NonlinearSolve
     OptimizedShooting(; kwargs...)
 
 A shooting method [Dednam2014](@cite) combined with Levenberg-Marquardt optimization 
-to find periodic orbits of continuous-time systems.
+to find periodic orbits of continuous-time dynamical systems.
 
 ## Keyword arguments
-- `Δt::Float64 = 1e-6`: step in the residual `R` between points. See below.
-- `n::Int64 = 2`: `n*dimension(ds)` is the number of points in the residual `R`. See below.
-- `optim_kwargs::NamedTuple = (x_tol=1e-10,)`: keyword arguments to pass to the optimizer. 
-  The optimizer used is the `optimize` from 
-  [`LeastSquaresOptim.jl`](https://github.com/matthieugomez/LeastSquaresOptim.jl). 
-  For details on the keywords see the respective package documentation.
-- `abstol::Float64 = 1e-3` : absolute tolerance for sum of squares `ssr` of the 
-  residual `R`. The method converged if `ssr <= abstol`.
+- `Δt::Float64 = 1e-6`: step between the points in the residual `R`. See below for details.
+- `n::Int64 = 2`: `n*dimension(ds)` is the number of points in the residual `R`. See below 
+  for details.
+- `nonlinear_solve_kwargs = (reltol=1e-6, abstol=1e-6, maxiters=1000)`: keyword arguments 
+  to pass to the `solve` function from 
+  [`NonlinearSolve.jl`](https://github.com/SciML/NonlinearSolve.jl). For details on the 
+  keywords see the respective package documentation.
 
 ## Description
 
-Continuous dynamical system 
+Let us consider the following continuous-time dynamical system
 
 ```math
 \\frac{dx}{dt} = f(x, p, t)
 ```
 
-can be rewritten in terms of the dimensionless time ``\\tau = t/T`` as
+Dednam and Botha [Dednam2014](@cite) suggest to minimize the residual ``R`` defined as
 
 ```math
-\\frac{dx}{d\\tau} = Tf(x, p, T\\tau)
+R = (x(T)-x(0), x(T+\\Delta t)-x(\\Delta t), \\dots, 
+x(T+(n-1)\\Delta t)-x((n-1)\\Delta t))
 ```
-where ``T`` is a period of some periodic orbit. The boundary conditions for the 
-periodic orbit now are ``x(\\tau = 0) = x(\\tau = 1)``. Dednam and Botha [Dednam2014](@cite) 
-suggest minimizing the residual ``R`` defined as 
-
-```math
-R = (x(1)-x(0), x(1+\\Delta \\tau)-x(\\Delta \\tau), \\dots, 
-x(1+(n-1)\\Delta \\tau)-x((n-1)\\Delta \\tau))
-```
-
- with respect to ``x`` and ``T`` using Levenberg-Marquardt optimization.
+where ``T`` is unknown period of a periodic orbit and ``x(t)`` is a solution at time ``t`` 
+given some unknown initial point. Initial guess of the period ``T`` and the initial point 
+is optimized by the Levenberg-Marquardt algorithm.
 
 In our implementation keyword argument `n` corresponds to ``n`` in the residual ``R``. 
-The keyword argument `Δt` corresponds to ``\\Delta \\tau`` in the residual ``R``.
+The keyword argument `Δt` corresponds to ``\\Delta t`` in the residual ``R``.
 """
 @kwdef struct OptimizedShooting{T} <: PeriodicOrbitFinder
     Δt::Float64 = 1e-6
     n::Int64 = 2
-    optim_kwargs::T = (x_tol=1e-8, g_tol=1e-3, f_tol=1e-6)
-    abstol::Float64 = 1e-3
+    nonlinear_solve_kwargs::T = (reltol=1e-6, abstol=1e-6, maxiters=1000)
 end
 
 function periodic_orbit(ds::CoupledODEs, alg::OptimizedShooting, ig::InitialGuess)
-    prob = NonlinearSolve.NonlinearLeastSquaresProblem(
-        NonlinearSolve.NonlinearFunction(costfunc(ds, alg), resid_prototype = zeros(alg.n*dimension(ds))), [ig.u0..., ig.T])
-    res = NonlinearSolve.solve(prob, NonlinearSolve.LevenbergMarquardt(), reltol = 1e-10, abstol = 1e-10)
-    if res.retcode == 1
-        u0 = res.u[1:dimension(ds)]
-        T = res.u[end]
-        return PeriodicOrbit(ds, u0, T, 0.01; jac=nothing)
-    end
-    return nothing
-end
+    D = dimension(ds)
+    f = (err, v, p) -> begin
+        if isinplace(ds) 
+            u0 = @view v[1:D]
+        else
+            u0 =  SVector{D}(v[1:D])
+        end
+        T = v[end]
 
-function periodic_orbit2(ds::CoupledODEs, alg::OptimizedShooting, ig::InitialGuess)
-    R1 = zeros(alg.n*dimension(ds))
-    R2 = copy(R1)
-    err = copy(R1)
-    cf = CostFunc(R1, R2, err)
-    res = optimize(v -> cf(v, ds, alg), [ig.u0..., ig.T], LevenbergMarquardt(); alg.optim_kwargs...)
-    if res.ssr <= alg.abstol
-        u0 = res.minimizer[1:dimension(ds)]
-        T = res.minimizer[end]
-        return PeriodicOrbit(ds, u0, T, 0.01; jac=nothing)
+        bounds = zeros(eltype(v), alg.n*2)
+        for i in 0:alg.n-1
+            bounds[i+1] = i*alg.Δt
+            bounds[i+alg.n+1] = T + i*alg.Δt
+        end
+        tspan = (0.0, T + alg.n*alg.Δt)
+
+        sol = solve(SciMLBase.remake(ds.integ.sol.prob; u0=u0, 
+        tspan=tspan); DynamicalSystemsBase.DEFAULT_DIFFEQ..., ds.diffeq..., saveat=bounds)
+        if (length(sol.u) == alg.n*2)
+            for i in 1:alg.n
+                err[D*i-(D-1):D*i] = (sol.u[i] - sol.u[i+alg.n])
+            end
+        else
+            fill!(err, Inf)
+        end
     end
-    return nothing
+
+    prob = NonlinearLeastSquaresProblem(
+        NonlinearFunction(f, resid_prototype = zeros(alg.n*dimension(ds))), [ig.u0..., ig.T])
+
+    sol = solve(prob, NonlinearSolve.LevenbergMarquardt(); alg.nonlinear_solve_kwargs...)
+    if sol.retcode == ReturnCode.Success
+        u0 = sol.u[1:end-1]
+        T = sol.u[end]
+        Δt = 0.1
+        return PeriodicOrbit(ds, u0, T, Δt)
+    else
+        return nothing
+    end
 end
 
 function periodic_orbits(ds::CoupledODEs, alg::OptimizedShooting, igs::Vector{<:InitialGuess})
-    # TODO: annotate `pos` with correct type
     pos = []
     for ig in igs
         res = periodic_orbit(ds, alg, ig)
@@ -90,65 +96,4 @@ function periodic_orbits(ds::CoupledODEs, alg::OptimizedShooting, igs::Vector{<:
         end
     end
     return pos
-end
-
-function periodic_orbits2(ds::CoupledODEs, alg::OptimizedShooting, igs::Vector{<:InitialGuess})
-    # TODO: annotate `pos` with correct type
-    pos = []
-    for ig in igs
-        res = periodic_orbit2(ds, alg, ig)
-        if !isnothing(res)
-            push!(pos, res)
-        end
-    end
-    return pos
-end
-
-struct CostFunc{T}
-    R1::Vector{T}
-    R2::Vector{T}
-    err::Vector{T}
-end
-
-function (cf::CostFunc)(v, ds, alg)
-    (; R1, R2, err) = cf
-    u0 = @view v[1:dimension(ds)]
-    T = v[end]
-    reinit!(ds, u0)
-    compute_residual!(R1, ds, u0, T*alg.Δt, alg.n, 0)
-    step!(ds, T - current_time(ds), true)
-    compute_residual!(R2, ds, u0, T*alg.Δt, alg.n, T)
-    err .= R2 .- R1
-    return err
-end
-
-function costfunc(ds, alg)
-    return (err, v, p) -> begin
-        u0 = v[1:dimension(ds)]
-        T = v[end]
-        R1 = compute_residual(ds, u0, T*alg.Δt, alg.n, 0)
-        R2 = compute_residual(ds, u0, T*alg.Δt, alg.n, T)
-        err .= R2 .- R1
-        return nothing
-    end
-end
-
-function compute_residual(ds, u0, Δt, n, t0)
-    len = n * dimension(ds)
-    R = zeros(len)
-
-    reinit!(ds.integ, u0)
-    step!(ds.integ, t0, true)
-    for i in 0:n-1
-        R[i*dimension(ds)+1:(i+1)*dimension(ds)] .= current_state(ds)
-        step!(ds.integ, Δt, true)
-    end
-    return R
-end
-
-function compute_residual!(R, ds, u0, Δt, n, t0)
-    for i in 0:n-1
-        R[i*dimension(ds)+1:(i+1)*dimension(ds)] .= current_state(ds)
-        step!(ds, Δt, true)
-    end
 end
--- test/algorithms/optimized_shooting.jl
+++ test/algorithms/optimized_shooting.jl
@@ -12,11 +12,10 @@ end
 @testset "Optimized shooting" begin
     igs = [InitialGuess(SVector(1.0, 2.0, 5.0), 4.2), InitialGuess(SVector(1.0, 2.0, 5.0), 5.2)]
     ig = igs[1]
-    alg = OptimizedShooting(Δt=1e-3, n=3, abstol=1e-6, optim_kwargs=(f_tol=1e-10,))
-    ds = CoupledODEs(lorenz_rule, [0.0, 10.0, 0.0], [10.0, 28.0, 8 / 3]; diffeq=(abstol=1e-14, reltol=1e-14))
+    alg = OptimizedShooting(Δt=1e-3, n=3, nonlinear_solve_kwargs=(abstol=1e-6, reltol=1e-6))
+    ds = CoupledODEs(lorenz_rule, [0.0, 10.0, 0.0], [10.0, 28.0, 8 / 3]; diffeq=(abstol=1e-10, reltol=1e-10))
 
-    @test res = periodic_orbit(ds, alg, ig)
-    @time res = periodic_orbit2(ds, alg, ig)
+    res = periodic_orbit(ds, alg, ig)
 
     @test !isnothing(res)
     if !isnothing(res)
@@ -28,7 +27,6 @@ end
     end
 
     res = periodic_orbits(ds, alg, igs)
-    res2 = periodic_orbits2(ds, alg, igs)
     @test length(res) == 2
 
     for po in res
@@ -38,32 +36,4 @@ end
         step!(ds, T, true)
         @test norm(current_state(ds) - u0) <= 1e-6
     end
-end
-
-#%%
-igs = [InitialGuess(SVector(1.0, 2.0, 5.0), 4.2), InitialGuess(SVector(1.0, 2.0, 5.0), 5.2)]
-ig = igs[1]
-alg = OptimizedShooting(Δt=1e-3, n=3, abstol=1e-6, optim_kwargs=(f_tol=1e-10,))
-ds = CoupledODEs(lorenz_rule, [0.0, 10.0, 0.0], [10.0, 28.0, 8 / 3]; diffeq=(abstol=1e-14, reltol=1e-14))
-
-@time res = periodic_orbit2(ds, alg, ig)
-
-@test !isnothing(res)
-if !isnothing(res)
-    u0 = res.points[1]
-    T = res.T
-    reinit!(ds, u0)
-    step!(ds, T, true)
-    @test norm(current_state(ds) - u0) <= 1e-6
-end
-
-@btime res = periodic_orbits2(ds, alg, igs)
-@test length(res) == 2
-
-for po in res
-    u0 = po.points[1]
-    T = po.T
-    reinit!(ds, u0)
-    step!(ds, T, true)
-    @test norm(current_state(ds) - u0) <= 1e-6
 end
