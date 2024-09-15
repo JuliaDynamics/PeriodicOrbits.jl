e325e930c7e2bcafac8f74db40ba269d1e8a98b3
Jonáš Koziorek <73384756+JonasKoziorek@users.noreply.github.com>
2024-09-15 14:41:18 +0200

    revert docs workaround (#11)

--- docs/make.jl
+++ docs/make.jl
@@ -8,11 +8,11 @@ pages = [
     "references.md"
 ]
 
-# import Downloads
-# Downloads.download(
-#     "https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/build_docs_with_style.jl",
-#     joinpath(@__DIR__, "build_docs_with_style.jl")
-# )
+import Downloads
+Downloads.download(
+    "https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/build_docs_with_style.jl",
+    joinpath(@__DIR__, "build_docs_with_style.jl")
+)
 include("build_docs_with_style.jl")
 
 using DocumenterCitations
