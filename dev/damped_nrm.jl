5eb2f8ba8fb3b2204d3f159dbb7a9f49f157ef0f
Jonas Koziorek <jonas.koziorek@gmail.com>
2024-09-19 16:05:53 +0200

    prepare file for gist

--- dev/damped_nrm.jl
+++ dev/damped_nrm.jl
@@ -1,4 +1,108 @@
 using PeriodicOrbits
+using LinearAlgebra: I, norm, SingularException
+
+@kwdef struct DampedNewtonRaphsonMees <: PeriodicOrbitFinder
+    δ = 2^(-3)
+    J = nothing
+    maxiter = 10000
+    disttol = 1e-4
+    maxperiod = 50
+    inftol = 1e7
+    checkperiod = 1.0
+end
+
+function periodic_orbits(ds::CoupledODEs, alg::PeriodicOrbitFinder, igs::Vector{InitialGuess})
+    tands = TangentDynamicalSystem(ds; J = alg.J, k=dimension(ds))
+    pos = PeriodicOrbit{typeof(current_state(ds)), typeof(current_time(ds))}[]
+
+    reverse_f(u, p, t) = -dynamic_rule(ds)(u, p, t)
+    reverse_J(u, p, t) = -alg.J(u, p, t)
+    reverse_ds = CoupledODEs(reverse_f, current_state(ds), current_parameters(ds))
+
+    reverse_tands = TangentDynamicalSystem(reverse_ds; J = reverse_J, k=dimension(ds))
+
+    dim = dimension(ds)+1
+
+    j = 0
+    for ig in igs
+        j+=1
+        println("Initial guess $j")
+        try
+            prev_step = SVector{length(ig.u0)+1}(vcat(ig.u0, ig.T))
+
+            if prev_step[end] >= 0.0
+                ds2 = tands
+            else
+                ds2 = reverse_tands
+            end
+
+            for _ in 1:alg.maxiter
+                reinit!(ds2, prev_step[1:end-1])
+                step!(ds2, abs(prev_step[end]), true)
+
+                if norm(current_state(ds2) - prev_step[1:end-1]) < alg.disttol
+                    push!(pos, PeriodicOrbit{typeof(current_state(ds)), typeof(current_time(ds))}([prev_step[1:end-1]], prev_step[end]))
+                    break
+                end
+
+                prev_step, status = next_step(prev_step, ds2, alg, dim)
+                status == false && break
+            end
+        catch e
+            @warn e.msg
+        end
+    end
+    return pos
+end
+
+function next_step(prev_step, tands, alg, dim)
+    X = prev_step[1:end-1]
+    T = prev_step[end]
+    reinit!(tands, X)
+
+    accum_norms = ones(size(current_deviations(tands)))
+    Δt = 0.3
+    for _ in 0:Δt:T
+        step!(tands, Δt, true)
+        cdevs = collect(current_deviations(tands))
+        for (j, dev) in enumerate(cdevs)
+            n = norm(dev)
+            accum_norms[j] *= n
+            cdevs[j] /= n # normalize deviation by its norm, so that it is norm 1
+        end
+        set_deviations!(tands, cdevs)
+    end
+
+    Phi = current_deviations(tands) .* accum_norms
+    phi = current_state(tands)
+
+    F = tands.original_f # this won't work. In-place and out of place dynamical
+    # systems have differnt format for F. One is F(dphi, phi, p, t)
+    # and the other is F(phi, p, t) for the out of place.
+    # What you should do instead is define an API function
+    # rate_of_change(ds, phi, p, t)
+    # that internally uses the `isinplace(ds)` function to decide how to call F.
+
+    p = current_parameters(tands)
+    t = current_time(tands)
+
+    A = hcat(vcat(Phi-I, F(X, p, t)'), vcat(F(phi, p, t), 0))
+    b = vcat(X-phi, 0)
+
+    try
+        Δ = A\b
+        return prev_step + alg.δ*Δ, true
+    catch e
+        if e isa SingularException
+            return prev_step, false
+        else
+            rethrow(e)
+        end
+    end
+end
+    
+
+# --- testing it out ---
 
 function lorenz(u0=[0.0, 10.0, 0.0]; σ = 10.0, ρ = 28.0, β = 8/3)
     return CoupledODEs(lorenz_rule, u0, [σ, ρ, β], diffeq = (abstol = 1e-10, reltol = 1e-10))
@@ -18,6 +122,6 @@ ds = lorenz(SVector(1.0, 10.0, 3.0))
 alg = DampedNewtonRaphsonMees(δ=2^(-2), J=lorenz_jacob, maxiter=8000, disttol=1e-2)
 traj, t = trajectory(ds, 4; Dt=1.0)
 igs = InitialGuess[InitialGuess(x, 10*rand()) for x in traj]
-pos = periodic_orbits(ds, alg, igs)
+pos = periodic_orbits(ds, alg, igs) # very slow and sometimes breaks
 reinit!(ds, traj[end])
 display(pos)
